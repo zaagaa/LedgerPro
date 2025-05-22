ca00e4be40f576ebd846acfc5ed0a2093fde2486
zaagaa <zaagaa.team@gmail.com>
2025-05-22 21:16:22 +0530

    Auto update @ 22-05-2025 21.16.21.64

---
+++ purchase/forms_upload.py
@@ -0,0 +1,7 @@
+# from django import forms
+#
+# class MultiplePurchaseScanForm(forms.Form):
+#     images = forms.FileField(
+#         widget=forms.FileInput(attrs={'multiple': True, 'class': 'form-control'}),
+#         required=False
+#     )
